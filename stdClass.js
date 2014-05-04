7c4eeb13105764ddb260003d14185c65b05de02e
Christian Blaschke <mail@platdesign.de>
2014-05-04 14:00:49 +0200

    removed underscore

--- stdClass.js
+++ stdClass.js
@@ -1,37 +1,33 @@
 (function(window){
 	
 	var extend = function(obj) {
-		each(slice.call(arguments, 1), function(source) {
-			if (source) {
-				for (var prop in source) {
-					obj[prop] = source[prop];
-				}
+		var sources = Array.prototype.slice.call(arguments, 1);
+		for(s in sources) {
+			var source = sources[s];
+			for (var prop in source) {
+				obj[prop] = source[prop];
 			}
-		});
-		return obj;
+		}
+	    return obj;
 	};
-	
-	
 	var stdClass = function(){};
 	
 		stdClass.extend = function(protoProps, staticProps) {
 	
-			/* Borrowed from Backbone.js */
-
 			var parent = this;
 			var child;
 
 			// The constructor function for the new subclass is either defined by you
 			// (the "constructor" property in your `extend` definition), or defaulted
 			// by us to simply call the parent's constructor.
-			if (protoProps && _.has(protoProps, 'constructor')) {
+			if (protoProps && Object.prototype.hasOwnProperty.call(protoProps, 'constructor')) {
 				child = protoProps.constructor;
 			} else {
 				child = function(){ return parent.apply(this, arguments); };
 			}
 
 			// Add static properties to the constructor function, if supplied.
-			_.extend(child, parent, staticProps);
+			extend(child, parent, staticProps);
 
 			// Set the prototype chain to inherit from `parent`, without calling
 			// `parent`'s constructor function.
@@ -41,7 +37,7 @@
   		
 			// Add prototype properties (instance properties) to the subclass,
 			// if supplied.
-			if (protoProps) _.extend(child.prototype, protoProps);
+			if (protoProps) extend(child.prototype, protoProps);
   		
 			// Set a convenience property in case the parent's prototype is needed
 			// later.
@@ -50,13 +46,16 @@
 			return child;
 		};
 	
-	
+		window.stdClass = stdClass;
+
+
 		
-		window.stdClass = stdClass.extend({
+		window.stdClassWithEvents = stdClass.extend({
 			on:function(eventname, closure){
 				var events = this.events || (this.events = {});
 				var event = events[eventname] || (events[eventname] = []);
 				event.push(closure);
+				return this;
 			},
 			trigger:function(eventname) {
 				var events = this.events || (this.events = {});
@@ -69,10 +68,11 @@
 					var closure = event[i];
 					closure.apply(this, newArgs);
 				}
+				return this;
 			}
 		});
 	
 		
 	
-})(window);
+}(window));
 
