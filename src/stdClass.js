06475d0932548441da59d83b49138b6d6e08f1e9
Christian Blaschke <mail@platdesign.de>
2015-03-06 12:21:34 +0100

    borwserify support

--- src/stdClass.js
+++ src/stdClass.js
@@ -1,16 +1,4 @@
-(function (root, factory) {
-
-	var moduleName = 'stdClass';
-
-	if (typeof define === 'function' && define.amd) {
-		define([moduleName], factory);
-	} else if (typeof exports === 'object') {
-		module.exports = factory();
-	} else {
-		root[moduleName] = factory();
-	}
-
-}(this, function () {
+'use strict';
 
 
 	var extend = function(obj) {
@@ -21,46 +9,46 @@
 				obj[prop] = source[prop];
 			}
 		}
-	    return obj;
+		return obj;
 	};
 
 
 	var stdClass = function(){};
 
-		stdClass.extend = function(protoProps, staticProps) {
-			var parent = this;
-			var child;
+	stdClass.extend = function(protoProps, staticProps) {
+		var parent = this;
+		var child;
 
-			// The constructor function for the new subclass is either defined by you
-			// (the "constructor" property in your `extend` definition), or defaulted
-			// by us to simply call the parent's constructor.
-			if (protoProps && Object.prototype.hasOwnProperty.call(protoProps, 'constructor')) {
-				child = protoProps.constructor;
-			} else {
-				child = function(){ return parent.apply(this, arguments); };
-			}
+		// The constructor function for the new subclass is either defined by you
+		// (the "constructor" property in your `extend` definition), or defaulted
+		// by us to simply call the parent's constructor.
+		if (protoProps && Object.prototype.hasOwnProperty.call(protoProps, 'constructor')) {
+			child = protoProps.constructor;
+		} else {
+			child = function(){ return parent.apply(this, arguments); };
+		}
+
+		// Add static properties to the constructor function, if supplied.
+		extend(child, parent, staticProps);
+
+		// Set the prototype chain to inherit from `parent`, without calling
+		// `parent`'s constructor function.
+		var Surrogate = function(){ this.constructor = child; };
+		Surrogate.prototype = parent.prototype;
+		child.prototype = new Surrogate();
+
+		// Add prototype properties (instance properties) to the subclass,
+		// if supplied.
+		if (protoProps) extend(child.prototype, protoProps);
+
+		// Set a convenience property in case the parent's prototype is needed
+		// later.
+		child.__super__ = parent.prototype;
+		return child;
+	};
+	stdClass.helper = {
+		extend:extend
+	};
 
 
-			// Add static properties to the constructor function, if supplied.
-			extend(child, parent, staticProps);
-
-			// Set the prototype chain to inherit from `parent`, without calling
-			// `parent`'s constructor function.
-			var Surrogate = function(){ this.constructor = child; };
-			Surrogate.prototype = parent.prototype;
-			child.prototype = new Surrogate();
-
-			// Add prototype properties (instance properties) to the subclass,
-			// if supplied.
-			if (protoProps) extend(child.prototype, protoProps);
-				
-			// Set a convenience property in case the parent's prototype is needed
-			// later.
-			child.__super__ = parent.prototype;
-			return child;
-		};
-		stdClass.helper = {
-			extend:extend
-		};
-	return stdClass;
-}));
+module.exports = stdClass;
